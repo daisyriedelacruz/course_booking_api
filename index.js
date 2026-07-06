dd99458af69a1d9c7db8ce649c2b54997c675846
daisyrie.delacruz <daisyrie.delacruz@tuitt.com>
2026-07-06 13:16:18 +0800

    Update cors origins

--- index.js
+++ index.js
@@ -16,10 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
   // origin that is allowed to send requests
-  origin: [
-    "http://localhost:3000",
-    "https://course-booking-api-n8y7.onrender.com",
-  ],
+  origin: ["http://localhost:3000", "https://course-booking-jade.vercel.app/"],
   // allows credentials like authorization headers
   credentials: true,
   // Provides status code for successful request
